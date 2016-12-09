5c39947f29b19dca039a9ba7c8260d38fa3ecdf2
Dao Le Kham <dlkham@mytrax.co.jp>
2016-12-09 16:47:30 +0700

    up code

--- DMPlayer/app/src/main/java/com/lk/dmplayer/activities/AlbumAndArtisDetailsActivity.java
+++ DMPlayer/app/src/main/java/com/lk/dmplayer/activities/AlbumAndArtisDetailsActivity.java
@@ -2,6 +2,8 @@ package com.lk.dmplayer.activities;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.lk.dmplayer.R;
 
@@ -9,9 +11,31 @@ import com.lk.dmplayer.R;
  * Created by dlkham on 12/6/2016.
  */
 public class AlbumAndArtisDetailsActivity extends MainActivity {
+    Toolbar toolbar;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_albumandartisdetails);
         super.onCreate(savedInstanceState);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(0,0);
+        finish();
     }
 }
